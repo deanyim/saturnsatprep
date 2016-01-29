4bfc30319960f05db0815fc7fe35101aca044cb3
Daesun Yim <yimdaesun@gmail.com>
2016-01-29 17:27:20 -0500

    Converted LESS to SCSS and got the template working

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( classie.js )
+Rails.application.config.assets.precompile += %w( freelancer.js )
+Rails.application.config.assets.precompile += %w( cbpAnimatedHeader.js )
